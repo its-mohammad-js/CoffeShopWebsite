515f28c0bf259e1c94b7da9282ec80f7bc2cb125
mohamad arab <mohammadreactjs@gmail.com>
2025-04-20 06:39:51 +0330

    update menu

--- src/assets/Products.js
+++ src/assets/Products.js
@@ -14,7 +14,7 @@ const Productsinfo = [
     },
     thumbnailSrc: "/images/Products/hotChocolateThumbnail.jpg",
     id: "prod-7f92b6c1-48f4-4f19-b6fa-e2c1c3a8a9d5",
-    price: "120,000",
+    price: "110,000",
   },
   {
     name: {
@@ -31,7 +31,7 @@ const Productsinfo = [
     },
     thumbnailSrc: "/images/Products/MasalaChaiThumbnail.jpg",
     id: "prod-7f92b6c1-38f4-4f19-b6fa-e2c1c3a8B9d5",
-    price: "120,000",
+    price: "95,000",
   },
   {
     name: {
@@ -48,7 +48,7 @@ const Productsinfo = [
     },
     thumbnailSrc: "/images/Products/KarakChaiThumbnail.jpg",
     id: "prod-7f92b6c1-38f4-4f19-b6fa-e2FFF1c3a8a9d5",
-    price: "120,000",
+    price: "95,000",
   },
   {
     name: {
@@ -65,7 +65,296 @@ const Productsinfo = [
     },
     thumbnailSrc: "/images/Products/ChaiThumbnail.jpg",
     id: "prod-7f92b6c1-38f4-4f19-b6fa-e2c1c3a8a9d5",
-    price: "120,000",
+    price: "45,000",
+  },
+  {
+    name: {
+      en: "Saffron Latte",
+      fa: "کافه زعفرانی",
+    },
+    category: {
+      en: "Hot Drinks",
+      fa: "نوشیدنی‌های گرم",
+    },
+    desc: {
+      en: "A luxurious latte infused with the exotic flavor of saffron, offering a rich and aromatic experience.",
+      fa: "کافه لاته‌ای لوکس که با طعم منحصر به فرد زعفران ترکیب شده و تجربه‌ای غنی و معطر به شما ارائه می‌دهد.",
+    },
+    thumbnailSrc: "/images/Products/SaffronLatteThumbnail.jpg",
+    id: "prod-7f92b6c1-38f4-4f19-345AVC-e2c1c3a8a9d5",
+    price: "98,000",
+  },
+  {
+    name: {
+      en: "Golden Milk",
+      fa: "شیر زعفران",
+    },
+    category: {
+      en: "Hot Drinks",
+      fa: "نوشیدنی‌های گرم",
+    },
+    desc: {
+      en: "A warm, creamy drink made with turmeric, milk, and a blend of spices. Known for its health benefits and soothing properties.",
+      fa: "نوشیدنی گرم و کرمی که با زردچوبه، شیر و ترکیبی از ادویه‌ها تهیه می‌شود. این نوشیدنی به خاطر فواید سلامتی و خواص آرامبخش خود شناخته شده است.",
+    },
+    thumbnailSrc: "/images/Products/GoldenMilkThumbnail.jpg",
+    id: "prod-7f92b6c1-38fEACASd19-345AVC-e2c1c3a8a9d5",
+    price: "73,000",
+  },
+  {
+    name: {
+      en: "Honey Milk",
+      fa: "شیر عسل",
+    },
+    category: {
+      en: "Hot Drinks",
+      fa: "نوشیدنی‌های گرم",
+    },
+    desc: {
+      en: "A warm and soothing drink made with milk and honey.",
+      fa: "یک نوشیدنی گرم و آرامش‌بخش ساخته شده از شیر و عسل.",
+    },
+    thumbnailSrc: "/images/Products/HoneyMilkThumbnail.jpg",
+    id: "prod-7f92b6c1-38f4-4f19-345AVC-e2cDSA432a8a9d5",
+    price: "78,000",
+  },
+  {
+    name: {
+      en: "Macchiato",
+      fa: "ماچالته",
+    },
+    category: {
+      en: "Hot Drinks",
+      fa: "نوشیدنی‌های گرم",
+    },
+    desc: {
+      en: "Macchiato is a rich espresso drink with a small amount of steamed milk and a delicate layer of milk foam on top. It offers a bold coffee flavor, balanced by the creamy texture of the milk foam. Perfect for those who enjoy a strong coffee with a hint of sweetness.",
+      fa: "ماچالته یک نوشیدنی اسپرسو غنی است که مقداری کمی شیر بخار داده شده و لایه‌ای ظریف از کف شیر بر روی آن قرار دارد. این نوشیدنی طعم قوی قهوه را با بافت کرمی کف شیر متعادل می‌کند. مناسب برای کسانی که قهوه‌ای قوی با یک طعم شیرین ملایم را دوست دارند.",
+    },
+    thumbnailSrc: "/images/Products/MacchiatoThumbnail.jpg",
+    id: "prod-unique-id-123456", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "145,000",
+  },
+  {
+    name: {
+      en: "Matcha",
+      fa: "ماچا",
+    },
+    category: {
+      en: "Hot Drinks",
+      fa: "نوشیدنی‌های گرم",
+    },
+    desc: {
+      en: "Matcha is a traditional Japanese green tea made from finely ground powder of specially grown green tea leaves. It has a vibrant, rich flavor with earthy undertones and is known for its calming and energizing properties. Perfect for tea lovers looking for a unique, healthy drink.",
+      fa: "ماچا چای سبز سنتی ژاپنی است که از پودر بسیار ریز برگ‌های چای سبز مخصوص کاشته شده تهیه می‌شود. این نوشیدنی طعمی غنی و پررنگ با زیرلحن‌های خاکی دارد و به خاطر خواص آرامش‌بخش و انرژی‌زا شناخته شده است. مناسب برای علاقه‌مندان به چای که به دنبال یک نوشیدنی سالم و منحصر به فرد هستند.",
+    },
+    thumbnailSrc: "/images/Products/MatchaThumbnail.jpg",
+    id: "prod-unique-id-734012", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "148,000",
+  },
+  {
+    name: {
+      en: "Snickers",
+      fa: "اسنیکرز",
+    },
+    category: {
+      en: "Sweets",
+      fa: "شیرینی‌ها",
+    },
+    desc: {
+      en: "Snickers is a delicious chocolate bar filled with caramel, nougat, and roasted peanuts, offering a perfect combination of sweet and savory flavors. With its crunchy texture and satisfying taste, it’s an irresistible treat for anyone with a sweet tooth or craving for something indulgent.",
+      fa: "اسنیکرز یک شکلات خوشمزه است که با کارامل، نوگت و بادام زمینی بو داده پر شده است و ترکیبی کامل از طعم‌های شیرین و شور را ارائه می‌دهد. با بافت ترد و طعم دلپذیرش، این یک انتخاب غیرقابل مقاومت برای کسانی است که به دسر شیرین علاقه دارند یا به چیزی لذیذ نیاز دارند.",
+    },
+    thumbnailSrc: "/images/Products/SnickersThumbnail.jpg",
+    id: "prod-unique-id-567890", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "145,000",
+  },
+  {
+    name: {
+      en: "Coffee Shake",
+      fa: "شیک قهوه",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Coffee Shake is a rich and creamy cold beverage made with freshly brewed coffee, milk, and ice. Blended to perfection, this drink offers a smooth coffee flavor with a refreshing chill. Ideal for coffee lovers who want to enjoy their favorite drink in a cool and indulgent form.",
+      fa: "شیک قهوه یک نوشیدنی سرد و کرمی است که از قهوه تازه دم شده، شیر و یخ تهیه می‌شود. این نوشیدنی به طرز عالی مخلوط شده و طعم قهوه‌ای نرم با خنکای دلپذیر ارائه می‌دهد. این نوشیدنی برای علاقه‌مندان به قهوه مناسب است که می‌خواهند نوشیدنی مورد علاقه‌شان را به صورت خنک و لذیذ تجربه کنند.",
+    },
+    thumbnailSrc: "/images/Products/CoffeeShakeThumbnail.jpg",
+    id: "prod-unique-id-671901", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "145,000",
+  },
+  {
+    name: {
+      en: "Nutella Shake",
+      fa: "شیک نوتلا",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Nutella Shake is a rich and indulgent cold drink made with creamy Nutella spread, milk, and ice. This delicious shake blends the smooth, hazelnut-chocolate flavor of Nutella with the refreshing chill of ice, creating a perfect treat for those who love sweet and creamy beverages.",
+      fa: "شیک نوتلا یک نوشیدنی سرد و لذیذ است که از نوتلا کرمی، شیر و یخ تهیه می‌شود. این شیک خوشمزه طعم نرم و شکلاتی فندق نوتلا را با خنکای یخ ترکیب کرده و یک دسر عالی برای کسانی است که نوشیدنی‌های شیرین و کرمی را دوست دارند.",
+    },
+    thumbnailSrc: "/images/Products/NutellaShakeThumbnail.jpg",
+    id: "prod-unique-id-789012", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "152,000",
+  },
+  {
+    name: {
+      en: "Oreo Shake",
+      fa: "شیک اورئو",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Oreo Shake is a creamy and indulgent cold drink made with crushed Oreo cookies, milk, and ice. This delicious shake combines the rich chocolate flavor of Oreos with a smooth, creamy texture, offering a delightful treat for anyone who loves this iconic cookie.",
+      fa: "شیک اورئو یک نوشیدنی سرد و کرمی است که از بیسکوییت اورئو خرد شده، شیر و یخ تهیه می‌شود. این شیک خوشمزه طعم غنی شکلاتی اورئو را با بافت نرم و کرمی ترکیب کرده و یک دسر لذت‌بخش برای کسانی است که این بیسکوییت نمادین را دوست دارند.",
+    },
+    thumbnailSrc: "/images/Products/OreoShakeThumbnail.jpg",
+    id: "prod-unique-id-890123Zs12", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "148,000",
+  },
+  {
+    name: {
+      en: "Lotus Shake",
+      fa: "شیک لوتوس",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Lotus Shake is a creamy and delightful cold drink made with Lotus Biscoff cookies, milk, and ice. This indulgent shake blends the caramelized flavor of Lotus cookies with a smooth, creamy texture, offering a sweet and satisfying treat for those who love unique and flavorful beverages.",
+      fa: "شیک لوتوس یک نوشیدنی سرد و کرمی است که از بیسکوییت لوتوس، شیر و یخ تهیه می‌شود. این شیک لذیذ طعم کاراملی بیسکوییت لوتوس را با بافت نرم و کرمی ترکیب کرده و یک دسر شیرین و رضایت‌بخش برای کسانی است که نوشیدنی‌های منحصر به فرد و خوش‌طعم را دوست دارند.",
+    },
+    thumbnailSrc: "/images/Products/LotusShakeThumbnail.jpg",
+    id: "prod-unique-id-901234ERAB123", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "151,000",
+  },
+  {
+    name: {
+      en: "Jif Shake",
+      fa: "شیک جیف",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Jif Shake is a rich and creamy cold drink made with smooth Jif peanut butter, milk, and ice. This delicious shake delivers a bold peanut flavor with a velvety texture, perfect for peanut butter lovers looking for a cool and satisfying treat.",
+      fa: "شیک جیف یک نوشیدنی سرد و غلیظ است که از کره بادام‌زمینی جیف، شیر و یخ تهیه می‌شود. این شیک خوشمزه طعمی قوی از کره بادام‌زمینی با بافتی نرم و ابریشمی ارائه می‌دهد و گزینه‌ای عالی برای علاقه‌مندان به طعم کره بادام‌زمینی در قالب یک نوشیدنی خنک و لذت‌بخش است.",
+    },
+    thumbnailSrc: "/images/Products/JifShakeThumbnail.jpg",
+    id: "prod-unique-id-012345VCA2389", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "150,000",
+  },
+  {
+    name: {
+      en: "Cheesecake Shake",
+      fa: "شیک پنیر",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "Cheesecake Shake is a smooth and decadent cold drink inspired by the classic cheesecake dessert. Made with creamy cheese, milk, and ice, this shake offers a rich and tangy flavor with a silky texture, perfect for those who enjoy bold and unique flavors.",
+      fa: "شیک پنیر یک نوشیدنی سرد و خوشمزه است که از دسر کلاسیک چیزکیک الهام گرفته شده است. با ترکیبی از پنیر خامه‌ای، شیر و یخ، این شیک طعمی غنی و کمی ترش با بافتی نرم و لطیف دارد و انتخابی متفاوت برای دوست‌داران طعم‌های خاص و منحصر‌به‌فرد است.",
+    },
+    thumbnailSrc: "/images/Products/CheesecakeShakeThumbnail.jpg",
+    id: "prod-unique-id-123456EZop0912", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "130,000",
+  },
+  {
+    name: {
+      en: "Crispy Bacon Sandwich",
+      fa: "ساندویچ خشک بیکن",
+    },
+    category: {
+      en: "Sandwiches",
+      fa: "ساندویچ‌ها",
+    },
+    desc: {
+      en: "Crispy Bacon Sandwich is a savory delight made with perfectly cooked, crunchy bacon layered between fresh bread. With its rich smoky flavor and satisfying texture, it's an ideal choice for those who love bold, meaty bites without the extra sauces.",
+      fa: "ساندویچ خشک بیکن یک انتخاب خوش‌طعم و کلاسیک است که از بیکن ترد و خوش‌عطر، در میان نان تازه تهیه می‌شود. این ساندویچ با طعم دودی و بافت دلچسب، مناسب افرادی است که طعم گوشت را بدون افزودنی‌های اضافی و سس ترجیح می‌دهند.",
+    },
+    thumbnailSrc: "/images/Products/CrispyBaconSandwichThumbnail.jpg",
+    id: "prod-unique-id-234567zwZ34", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "208,000",
+  },
+  {
+    name: {
+      en: "Turkey Sandwich",
+      fa: "ساندویچ بوقلمون",
+    },
+    category: {
+      en: "Sandwiches",
+      fa: "ساندویچ‌ها",
+    },
+    desc: {
+      en: "Turkey Sandwich is a light and flavorful choice made with tender slices of roasted turkey breast layered in fresh bread. Perfect for a hearty yet healthy meal, this sandwich offers a balanced taste with lean protein and a soft, satisfying bite.",
+      fa: "ساندویچ بوقلمون انتخابی سبک و خوش‌طعم است که از برش‌های لطیف سینه بوقلمون کبابی در نان تازه تهیه می‌شود. این ساندویچ با طعمی متعادل و پروتئین کم‌چرب، گزینه‌ای عالی برای یک وعده سالم و سیرکننده است.",
+    },
+    thumbnailSrc: "/images/Products/TurkeySandwichThumbnail.jpg",
+    id: "prod-unique-id-345678", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "217,000",
+  },
+  {
+    name: {
+      en: "Cookie",
+      fa: "کوکی",
+    },
+    category: {
+      en: "Desserts",
+      fa: "دسرها",
+    },
+    desc: {
+      en: "Freshly baked cookie with a soft center and slightly crispy edges. Made with premium ingredients like chocolate chips and real butter, this classic treat is perfect alongside your favorite drink or enjoyed on its own.",
+      fa: "کوکی تازه‌پخت با مرکز نرم و لبه‌های کمی ترد، تهیه‌شده از مواد اولیه باکیفیت مانند چیپس شکلات و کره طبیعی. این شیرینی کلاسیک، انتخابی عالی برای همراهی با نوشیدنی دلخواه یا لذت‌بردن به تنهایی است.",
+    },
+    thumbnailSrc: "/images/Products/Cookiethumbnail.jpg",
+    id: "prod-unique-id-456789mmzw12", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "70,000",
+  },
+  {
+    name: {
+      en: "Jar Cake",
+      fa: "جار کیک",
+    },
+    category: {
+      en: "Desserts",
+      fa: "دسرها",
+    },
+    desc: {
+      en: "Jar Cake is a beautifully layered dessert served in a jar, featuring moist cake, rich cream, and flavorful toppings. Perfect for on-the-go indulgence or a sweet gift, this treat combines texture and taste in every spoonful.",
+      fa: "جار کیک دسری خوشمزه و لایه‌لایه است که در یک جار شیشه‌ای سرو می‌شود و ترکیبی از کیک نرم، خامه غنی و تاپینگ‌های خوش‌طعم را در خود دارد. انتخابی عالی برای لذت در حرکت یا یک هدیه شیرین، که در هر قاشق ترکیبی از بافت و طعم را ارائه می‌دهد.",
+    },
+    thumbnailSrc: "/images/Products/JarCakeThumbnail.jpg",
+    id: "prod-unique-id-517890zxWe", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "130,000",
+  },
+  {
+    name: {
+      en: "Soft Drink",
+      fa: "نوشابه",
+    },
+    category: {
+      en: "Cold Drinks",
+      fa: "نوشیدنی‌های سرد",
+    },
+    desc: {
+      en: "A classic carbonated soft drink, served chilled for maximum refreshment. Its fizzy texture and bold flavor make it a timeless choice to pair with any meal.",
+      fa: "نوشابه یک نوشیدنی گازدار کلاسیک است که به صورت خنک سرو می‌شود تا نهایت تازگی را ارائه دهد. با بافت گازدار و طعم قوی، انتخابی همیشگی برای همراهی با انواع غذاها است.",
+    },
+    thumbnailSrc: "/images/Products/SoftDrinkThumbnail.jpg",
+    id: "prod-unique-id-678901", // لطفاً یک شناسه منحصر به فرد برای محصول قرار دهید
+    price: "22,000",
   },
 ];
 
